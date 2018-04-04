4bb2df53ae1a9c0245fedb00ff78f5c6384ce253
LATBauerdick <github@latbauerdick.de>
2018-04-04 11:12:23 -0500

    added histogram example

--- src/Lib.hs
+++ src/Lib.hs
@@ -19,6 +19,47 @@ import Control.Lens
 import Data.Generics.Labels()
 import NumHask.Prelude
 
+makeHistDiffExample :: IO ([Rect Double], [Rect Double])
+makeHistDiffExample = do
+  g <- create
+  xys <- rvs g 1000
+  xys1 <- rvs g 1000
+  let cuts = grid OuterPos (Range -5.0 5.0) 50
+  pure
+    ( makeRects IgnoreOvers (fill cuts xys)
+    , makeRects IgnoreOvers (fill cuts ((1.5 *) <$> xys1)))
+
+histDiffExample :: ([Rect Double], [Rect Double]) -> Chart b
+histDiffExample (h1, h2) =
+  let deltah =
+        zipWith (\(Rect x y z w) (Rect _ _ _ w') -> Rect x y z (w - w')) h1 h2
+      mainAspect = Rect -0.75 0.75 -0.5 0.5
+      botAspect = Rect -0.75 0.75 -0.2 0.2
+      (Ranges rx ry) = fold $ fold [h1, h2]
+      (Ranges _ deltary) = fold (abs <$> deltah)
+  in D.pad 1.1 $
+     D.beside
+       (D.r2 (0, -1))
+       (rectChart
+          [ #borderColor .~ ucolor 0 0 0 0 $
+            #color .~ ucolor 0.365 0.647 0.855 0.2 $
+            def
+          , #borderColor .~ ucolor 0 0 0 0 $
+            #color .~ ucolor 0.88 0.53 0.23 0.8 $
+            def
+          ]
+          mainAspect
+          (Ranges rx ry)
+          [h1, h2])
+       (rectChart
+          [ #borderColor .~ ucolor 0 0 0 0 $
+            #color .~ ucolor 0.88 0.53 0.23 0.8 $
+            def
+          ]
+          botAspect
+          (Ranges rx deltary)
+          [deltah]) <>
+        hud def botAspect (Ranges rx deltary)
 
 
 someFunc :: IO ()
