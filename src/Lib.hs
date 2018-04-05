62e87fda850f8f02e5f21daa270ac2e9c58f82fd
LATBauerdick <github@bauerdick.org>
2018-04-05 16:39:44 +0000

    setup test i/s

--- src/Lib.hs
+++ src/Lib.hs
@@ -30,42 +30,43 @@ makeHistDiffExample = do
     ( makeRects IgnoreOvers (fill cuts xys)
     , makeRects IgnoreOvers (fill cuts ((1.5 *) <$> xys1)))
 
-histDiffExample :: ([Rect Double], [Rect Double]) -> Chart b
-histDiffExample (h1, h2) =
-  let deltah =
-        zipWith (\(Rect x y z w) (Rect _ _ _ w') -> Rect x y z (w - w')) h1 h2
-      mainAspect = Rect -0.75 0.75 -0.5 0.5
-      botAspect = Rect -0.75 0.75 -0.2 0.2
-      (Ranges rx ry) = fold $ fold [h1, h2]
-      (Ranges _ deltary) = fold (abs <$> deltah)
-  in D.pad 1.1 $
-     D.beside
-       (D.r2 (0, -1))
-       (rectChart
-          [ #borderColor .~ ucolor 0 0 0 0 $
-            #color .~ ucolor 0.365 0.647 0.855 0.2 $
-            def
-          , #borderColor .~ ucolor 0 0 0 0 $
-            #color .~ ucolor 0.88 0.53 0.23 0.8 $
-            def
-          ]
-          mainAspect
-          (Ranges rx ry)
-          [h1, h2])
-       (rectChart
-          [ #borderColor .~ ucolor 0 0 0 0 $
-            #color .~ ucolor 0.88 0.53 0.23 0.8 $
-            def
-          ]
-          botAspect
-          (Ranges rx deltary)
-          [deltah]) <>
-        hud def botAspect (Ranges rx deltary)
+-- histDiffExample :: ([Rect Double], [Rect Double]) -> Chart b
+-- histDiffExample (h1, h2) =
+--   let deltah =
+--         zipWith (\(Rect x y z w) (Rect _ _ _ w') -> Rect x y z (w - w')) h1 h2
+--       mainAspect = Rect -0.75 0.75 -0.5 0.5
+--       botAspect = Rect -0.75 0.75 -0.2 0.2
+--       (Ranges rx ry) = fold $ fold [h1, h2]
+--       (Ranges _ deltary) = fold (abs <$> deltah)
+--   in D.pad 1.1 $
+--      D.beside
+--        (D.r2 (0, -1))
+--        (rectChart
+--           [ #borderColor .~ ucolor 0 0 0 0 $
+--             #color .~ ucolor 0.365 0.647 0.855 0.2 $
+--             def
+--           , #borderColor .~ ucolor 0 0 0 0 $
+--             #color .~ ucolor 0.88 0.53 0.23 0.8 $
+--             def
+--           ]
+--           mainAspect
+--           (Ranges rx ry)
+--           [h1, h2])
+--        (rectChart
+--           [ #borderColor .~ ucolor 0 0 0 0 $
+--             #color .~ ucolor 0.88 0.53 0.23 0.8 $
+--             def
+--           ]
+--           botAspect
+--           (Ranges rx deltary)
+--           [deltah]) <>
+--         hud def botAspect (Ranges rx deltary)
 
 
 someFunc :: IO ()
-someFunc = do
-  putStrLn "someFunc"
+someFunc =
+  putStrLn ( "someFunc":: Text )
+
   -- fileSvg "scaleExample.svg" (#size .~ Pair 600 240 $ def) $
   -- withHud
   -- def
